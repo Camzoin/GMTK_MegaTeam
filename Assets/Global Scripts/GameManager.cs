b4d9467fac73eae4b13db68babdd8f73f65619ca
Alexzoin <alex.cecrle@gmail.com>
2020-07-10 17:24:35 -0500

    Created Game Manager

---
+++ Assets/Global Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance = null;
+
+	private void Awake()
+	{
+		//Make sure this is the only Game Manager
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+
+		GameObject existingGameManager = GameObject.Find("GameManager");
+		if (existingGameManager != gameObject)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
