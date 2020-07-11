0eaaa5ed088f7706726dd46f1edac07395a7c342
Camzoin <Camden.Cecrle@gmail.com>
2020-07-11 11:49:59 -0700

    Updated scene changer to work

--- Assets/Global Scripts/SceneChanger.cs
+++ Assets/Global Scripts/SceneChanger.cs
@@ -9,15 +9,17 @@ public class SceneChanger : MonoBehaviour
     float size = 0;
     float rotation = 45;
     bool fadingIn, fadingOut;
-    float alpha = 0;
+    float alpha;
     private int nextScene;
+    
 
     private void Awake()
     {
-        fadeMat.SetInt("Pattern1", 0);
-        fadeMat.SetInt("Pattern2", 0);
-        fadeMat.SetInt("Pattern3", 0);
-        fadeMat.SetInt("Pattern4", 0);
+        //fadeMat.SetInt("Pattern1", 0);
+        //fadeMat.SetInt("Pattern2", 0);
+        //fadeMat.SetInt("Pattern3", 0);
+        //fadeMat.SetInt("Pattern4", 0);
+        //alpha = 0;
     }
 
     // Start is called before the first frame update
@@ -27,18 +29,19 @@ public class SceneChanger : MonoBehaviour
         RandomPattern();
 
         fadeMat.SetFloat("Rotation", rotation);
-        fadeMat.SetInt("Pattern" + Random.Range(1, 4), 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+
         if (fadingOut == true)
         {
-            size = Mathf.Lerp(size, 1, Time.deltaTime);
+            size = Mathf.Lerp(size, 1.1f, 2 * Time.deltaTime);
             alpha = Mathf.Lerp(alpha, 1, 5 * Time.deltaTime);
 
-            if(Mathf.Approximately(size, 1))
+            if(size > 1)
             {
                 fadingOut = false;
                 fadingIn = true;
@@ -48,11 +51,12 @@ public class SceneChanger : MonoBehaviour
 
         if (fadingIn == true)
         {
-            size = Mathf.Lerp(size, 0, Time.deltaTime);
-            alpha = Mathf.Lerp(alpha, 0, 5* Time.deltaTime);
+            size = Mathf.Lerp(size, -0.1f, 0.5f * Time.deltaTime);
+            alpha = Mathf.Lerp(alpha, 0, 5 * Time.deltaTime);
 
-            if (Mathf.Approximately(size, 0))
+            if (size < 0)
             {
+                size = 0;
                 fadingIn = false;
             }
         }
