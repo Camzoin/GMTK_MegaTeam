8cdb07aed1a2ce2a68088060b8989678ece518fb
JusticeShultz <shultzjc1@gmail.com>
2020-07-11 00:28:21 -0700

    Pushing up bullet collision feature

--- Assets/Games/Aim Trainer/Scripts/Dummy.cs
+++ Assets/Games/Aim Trainer/Scripts/Dummy.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Dummy : MonoBehaviour
 {
     public bool Up = false;
+    private bool shot = false;
+
+    public float shotCD = 0.0f;
 
     void Update()
     {
         if (Up)
             transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.01f));
         else
-            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.01f));
+            if(shot)
+                transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.08f));
+            else
+                transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.01f));
+
+        shot = shotCD > 0;
+        shotCD -= Time.deltaTime;
     }
 }
