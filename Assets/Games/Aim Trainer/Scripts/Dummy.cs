85fbe32a3459bacb69b19366c8213f275fe27a56
Camzoin <Camden.Cecrle@gmail.com>
2020-07-11 12:53:55 -0700

    Revert "Added scoring to dummy.cs"
    This reverts commit b698586a36a55580dad6897ecd2faa9b6672dd3b.

--- Assets/Games/Aim Trainer/Scripts/Dummy.cs
+++ Assets/Games/Aim Trainer/Scripts/Dummy.cs
@@ -7,31 +7,15 @@ public class Dummy : MonoBehaviour
     public bool Up = false;
     private bool shot = false;
 
-    //CAMDEN DID THIS
-    bool pointScored = false;
-
     public float shotCD = 0.0f;
 
     void Update()
     {
         if (Up)
-        { 
-           transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.025f));
-
-           //CAMDEN DID THIS
-           pointScored = false;
-        }
-
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.025f));
         else
-            if (shot)
+            if(shot)
                 transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.08f));
-                //CAMDEN DID THIS
-                if (pointScored == false)
-                {
-                   GameManager.instance.ScorePoints(GameManager.games.AIMTRAIN, 1);
-                   
-                   pointScored = true;
-                }
             else
                 transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.025f));
 
