cf1ffc1641e8edd3a87bb1cec2ac83cd7071b352
JusticeShultz <shultzjc1@gmail.com>
2020-07-10 20:12:38 -0700

    Pushing up aim trainer prototype

---
+++ Assets/Games/Aim Trainer/Scripts/Dummy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public bool Up = false;
+
+    void Update()
+    {
+        if (Up)
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.01f));
+        else
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, new Vector3(90, 0, 0), 0.01f));
+    }
+}
