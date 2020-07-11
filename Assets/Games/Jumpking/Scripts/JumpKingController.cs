54a7984be4318eff0c415cfbc7c000df0fb7ce94
JusticeShultz <shultzjc1@gmail.com>
2020-07-11 10:56:24 -0700

    Player animates in jump king

--- Assets/Games/Jumpking/Scripts/JumpKingController.cs
+++ Assets/Games/Jumpking/Scripts/JumpKingController.cs
@@ -14,7 +14,8 @@ public class JumpKingController : MonoBehaviour
     public float cameraSmoothing = 1f;
     public float fillBarSmoothing = 1f;
     public bool grounded = false;
-    
+
+    public Animator animator;
     public GameObject cameraObject;
     public new Rigidbody2D rigidbody2D;
     public UnityEngine.UI.Image fillBar;
@@ -33,7 +34,7 @@ public class JumpKingController : MonoBehaviour
     {
         float inputAxis = Input.GetAxis("Horizontal");
 
-        Collider2D[] cast = Physics2D.OverlapBoxAll(new Vector2(transform.position.x, transform.position.y) + (Vector2.down * 0.75f), new Vector2(0.5f, 0.1f), 0f);
+        Collider2D[] cast = Physics2D.OverlapBoxAll(new Vector2(transform.position.x, transform.position.y) + (Vector2.down * 0.75f), new Vector2(0.7f, 0.1f), 0f);
         grounded = cast.Length != 0;
         
         if (inputAxis >= 0.05)
@@ -48,9 +49,12 @@ public class JumpKingController : MonoBehaviour
         if((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && grounded)
         {
             currentHeldCharge += Time.deltaTime * jumpChargeSpeed;
+            animator.SetBool("InputState", true);
         }
         else
-        {   
+        {
+            animator.SetBool("InputState", false);
+
             if (sprite.flipX)
                 rigidbody2D.AddForce(new Vector2(Mathf.Clamp(Time.deltaTime * currentHeldCharge * jumpLengthX, maxJumpDistance * -1, maxJumpDistance), Mathf.Clamp(Time.deltaTime * currentHeldCharge * jumpLengthY, maxJumpDistance * -1, maxJumpDistance)), ForceMode2D.Force);
 
@@ -60,6 +64,20 @@ public class JumpKingController : MonoBehaviour
             currentHeldCharge = 0f;
         }
 
+        int state = 0;
+
+        if (currentHeldCharge == 0f)
+            state = 0;
+        else if (currentHeldCharge < 0.4f)
+            state = 1;
+        else if (currentHeldCharge < 0.65f)
+            state = 2;
+        else if (currentHeldCharge > 0.75f)
+            state = 3;
+
+        animator.SetBool("Grounded", grounded);
+        animator.SetInteger("Jump", state);
+
         cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, transform.position + new Vector3(0, 5, -10), Time.deltaTime * cameraSmoothing);
 
         fillBar.fillAmount = Mathf.Lerp(fillBar.fillAmount, Mathf.Clamp(Mathf.Clamp(currentHeldCharge * jumpLengthX, 0, maxJumpDistance) / maxJumpDistance, 0, 1), Time.deltaTime * fillBarSmoothing);
@@ -67,7 +85,7 @@ public class JumpKingController : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawCube(transform.position + (Vector3.down * 0.75f), new Vector3(0.5f, 0.1f, 0.1f));
+        Gizmos.DrawCube(transform.position + (Vector3.down * 0.75f), new Vector3(0.7f, 0.1f, 0.1f));
     }
 
     public void Respawn()
