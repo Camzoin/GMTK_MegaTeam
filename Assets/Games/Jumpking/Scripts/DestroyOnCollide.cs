7405ccbed34a5aebff97fa51d7103d92ad2a7de2
JusticeShultz <shultzjc1@gmail.com>
2020-07-11 19:25:47 -0700

    Final polish to jump king

---
+++ Assets/Games/Jumpking/Scripts/DestroyOnCollide.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollide : MonoBehaviour
+{
+    public GameObject destroyEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (destroyEffect != null)
+            Instantiate(destroyEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
