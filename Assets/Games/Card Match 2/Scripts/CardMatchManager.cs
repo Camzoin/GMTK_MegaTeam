8325e6bd46ef8f9ce0d960da6adc7f183912bdd1
Carter <carter.patterson95@gmail.com>
2020-07-11 22:09:15 -0700

    CardGame Scripts

---
+++ Assets/Games/Card Match 2/Scripts/CardMatchManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMatchManager : MonoBehaviour
+{
+    public Vector2 cardDims, padding;
+
+    public GameObject cardPrefab;
+
+    public Card[,] cards = new Card[9,4];
+
+    public void NewGame()
+    {
+
+    }
+
+    public void GenerateDeck()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+
+            }
+        }
+    }
+}
