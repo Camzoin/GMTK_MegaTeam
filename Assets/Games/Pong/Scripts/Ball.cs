26aa8e322d8d73528867582cf8d34d5fd2f8f569
diammen <marcelnamdao@gmail.com>
2020-07-12 02:12:15 -0700

    Finished Pong

--- Assets/Games/Pong/Scripts/Ball.cs
+++ Assets/Games/Pong/Scripts/Ball.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
+    public GameObject bounceEffect;
+    public Image right, left;
+    public float moveSpeed;
+    public float maxSpeed;
+    public float xBounds;
+
     Rigidbody2D rb;
 
-    public float moveSpeed;
-    public float xBounds;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +33,11 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate()
     {
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
         if (rb.position.x > xBounds)
         {
+            StartCoroutine(FlashSide(left, 0.8f, 0, 1f));
+
             rb.position = Vector3.zero;
 
             Vector2 moveVec = Random.insideUnitCircle.normalized;
@@ -38,17 +46,35 @@ public class Ball : MonoBehaviour
         }
         if (rb.position.x < -xBounds)
         {
+            StartCoroutine(FlashSide(right, 0.8f, 0, 1f));
+
             rb.position = Vector3.zero;
 
             Vector2 moveVec = Random.insideUnitCircle.normalized;
 
             rb.velocity = moveVec * moveSpeed;
+
+            GameManager.instance.ScorePoints(GameManager.games.PONG, 1);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        rb.AddForce(Random.insideUnitCircle.normalized * 5, ForceMode2D.Impulse);
+        rb.AddForce(Random.insideUnitCircle.normalized * 3, ForceMode2D.Impulse);
         CameraShake.instance.Shake(0.1f);
+        Instantiate(bounceEffect, transform.position, Quaternion.identity);
+    }
+
+    IEnumerator FlashSide(Image image, float start, float end, float duration)
+    {
+        float timer = 0;
+
+        while (timer <= 1)
+        {
+            timer += Time.deltaTime / duration;
+            float newAlpha = Mathf.Lerp(start, end, Mathf.SmoothStep(0, 1, timer));
+            image.color = new Color(image.color.r, image.color.g, image.color.b, newAlpha);
+            yield return null;
+        }
     }
 }
