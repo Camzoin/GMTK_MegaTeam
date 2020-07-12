a73e626a8927e8bba21089ebb0d95c560fc5e218
Alexzoin <alex.cecrle@gmail.com>
2020-07-11 19:21:06 -0500

    Mole Updates

--- Assets/Games/Whack-a-mole/Scripts/HammerController.cs
+++ Assets/Games/Whack-a-mole/Scripts/HammerController.cs
@@ -11,29 +11,66 @@ public class HammerController : MonoBehaviour
 
 	private bool wasDownLastFrame = false;
 
+	private float maxHammerDownTime = 0.25f;
+	private float hammerDownAt;
+
 	// Start is called before the first frame update
 	void Start()
     {
 		yPos = transform.position.y;
-    }
 
-    // Update is called once per frame
-    void Update()
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Confined;
+
+	}
+
+	// Update is called once per frame
+	void Update()
     {
 		Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		//Vector3 mousePoint = Input.mousePosition;
+
+		Camera.main.transform.LookAt(transform.position);
+
+		if (mousePoint.x > 20f)
+		{
+			mousePoint.x = 20f;
+		}
+		else if (mousePoint.x < -20f)
+		{
+			mousePoint.x = -20f;
+		}
 
 		gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, mousePoint, hammerMoveSpeed);
-		transform.position = new Vector3(transform.position.x, yPos, (transform.position.z * 1.072f) - 4.75f);
+
+		transform.position = new Vector3(transform.position.x, yPos, (transform.position.z * 1.1f) - 4.75f);
+
+		if (transform.position.z > 18f)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, 18f);
+		}
+		else if (transform.position.z < -16f)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, -16f);
+		}
 
 		if (Input.GetMouseButton(0))
 		{
 			if (!wasDownLastFrame)
 			{
 				hammerSwish.Play();
+				hammerDownAt = Time.time;
 			}
-			wasDownLastFrame = true;
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(-105f, transform.rotation.y, transform.rotation.z)), 0.5f);
+
+			if (Time.time < hammerDownAt + maxHammerDownTime)
+			{
+				wasDownLastFrame = true;
+				transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(-105f, transform.rotation.y, transform.rotation.z)), 0.5f);
+			}
+			else
+			{
+				transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0f, transform.rotation.y, transform.rotation.z)), 0.2f);
+			}
+			
 		}
 		else
 		{
