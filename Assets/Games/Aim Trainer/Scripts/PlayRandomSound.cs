29c78a8cede588d8dd2338ccdf814c03be353841
JusticeShultz <shultzjc1@gmail.com>
2020-07-11 20:13:26 -0700

    Pushing up polish

---
+++ Assets/Games/Aim Trainer/Scripts/PlayRandomSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource audioSource = null;
+    public List<AudioClip> soundClips = new List<AudioClip>();
+
+    void OnEnable()
+    {
+        audioSource.PlayOneShot(soundClips[Random.Range(0, soundClips.Count)]);
+    }
+}
